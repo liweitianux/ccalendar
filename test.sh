4857ea7e67477b018e7e44e1a073a616000938a2
Aaron LI <aly@aaronly.me>
2019-12-04 22:07:38 +0800

    Define '_DEFAULT_SOURCE' on Linux

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ SRCS="basics.c chinese.c gregorian.c moon.c sun.c utils.c"
 CFLAGS="-std=c99 -O2 -Wall -Wextra"
 LDFLAGS="-lm"
 
+[ "$(uname -s)" = "Linux" ] && CFLAGS="${CFLAGS} -D_DEFAULT_SOURCE"
+
 gcc ${CFLAGS} -o test test.c ${SRCS} ${LDFLAGS} && ./test
