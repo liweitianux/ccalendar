127ab1926c3e4a578868d5211be98efafb381889
Aaron LI <aly@aaronly.me>
2020-01-24 22:11:28 +0800

    test.sh: Fix compilation by using libbsd

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 SRCS="basics.c chinese.c gregorian.c moon.c sun.c utils.c"
 SRCS="${SRCS} locale.c events.c dates.c parsedata.c io.c day.c ostern.c paskha.c pom.c sunpos.c"
 CFLAGS="-std=c99 -O2 -Wall -Wextra"
-LDFLAGS="-lm"
+CFLAGS="${CFLAGS} $(pkg-config --cflags libbsd-overlay)"
+LDFLAGS="-lm $(pkg-config --libs   libbsd-overlay)"
 
 [ "$(uname -s)" = "Linux" ] && CFLAGS="${CFLAGS} -D_DEFAULT_SOURCE"
 
