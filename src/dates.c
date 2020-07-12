087d63bdcbf2106159379df89c666a33c116ea3f
Aaron LI <aly@aaronly.me>
2020-07-12 18:54:05 +0800

    dates.c: Rewrite to use fixed date to simplify a lot

--- src/dates.c
+++ src/dates.c
@@ -1,8 +1,12 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
+ * Copyright (c) 2020 The DragonFly Project.  All rights reserved.
  * Copyright (c) 1992-2009 Edwin Groothuis <edwin@FreeBSD.org>.
  * All rights reserved.
+ *
+ * This code is derived from software contributed to The DragonFly Project
+ * by Aaron LI <aly@aaronly.me>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,7 +33,6 @@
  */
 
 #include <assert.h>
-#include <err.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,31 +42,6 @@
 #include "gregorian.h"
 #include "utils.h"
 
-struct cal_year {
-	int year;	/* 19xx, 20xx, 21xx */
-	struct cal_month *months;
-	struct cal_year *nextyear;
-};
-
-struct cal_month {
-	int month;			/* 01 .. 12 */
-	int firstdayjulian;		/* 000 .. 366 */
-	struct cal_year *year;		/* points back */
-	struct cal_day *days;
-	struct cal_month *nextmonth;
-};
-
-struct cal_day {
-	int dayofmonth;			/* 01 .. 31 */
-	int julianday;			/* 000 .. 366 */
-	int dayofweek;			/* 0 .. 6 */
-	struct cal_day *nextday;
-	struct cal_month *month;	/* points back */
-	struct cal_year *year;		/* points back */
-	struct event *events;
-};
-
-static struct cal_year *hyear = NULL;
 
 /* 1-based month, 0-based days, cumulative */
 int	cumdaytab[][14] = {
@@ -71,195 +49,63 @@ int	cumdaytab[][14] = {
 	{0, -1, 30, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365},
 };
 /* 1-based month, individual */
-static int *monthdays;
 int	monthdaytab[][14] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 30},
 	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 30},
 };
 
-static void createdate(int y, int m, int d);
+
+struct cal_day {
+	int		 rd;
+	struct cal_day	*next;
+	struct event	*events;
+};
+static struct cal_day *cal_days = NULL;
 
 
 int
 cal_day_get_month(struct cal_day *dayp)
 {
-	return dayp->month->month;
+	struct g_date gdate;
+
+	gregorian_from_fixed(dayp->rd, &gdate);
+	return gdate.month;
 }
 
 int
 cal_day_get_day(struct cal_day *dayp)
 {
-	return dayp->dayofmonth;
+	struct g_date gdate;
+
+	gregorian_from_fixed(dayp->rd, &gdate);
+	return gdate.day;
 }
 
-
-static void
-createdate(int y, int m, int d)
-{
-	struct cal_year *py, *pyp;
-	struct cal_month *pm, *pmp;
-	struct cal_day *pd, *pdp;
-	int *cumday;
-
-	pyp = NULL;
-	py = hyear;
-	while (py != NULL) {
-		if (py->year == y)
-			break;
-		pyp = py;
-		py = py->nextyear;
-	}
-
-	if (py == NULL) {
-		py = xcalloc(1, sizeof(*py));
-		py->year = y;
-		if (pyp != NULL)
-			pyp->nextyear = py;
-	}
-	if (hyear == NULL)
-		hyear = py;
-
-	pmp = NULL;
-	pm = py->months;
-	while (pm != NULL) {
-		if (pm->month == m)
-			break;
-		pmp = pm;
-		pm = pm->nextmonth;
-	}
-
-	if (pm == NULL) {
-		pm = xcalloc(1, sizeof(*pm));
-		pm->year = py;
-		pm->month = m;
-		cumday = cumdaytab[isleap(y)];
-		pm->firstdayjulian = cumday[m] + 2;
-		if (pmp != NULL)
-			pmp->nextmonth = pm;
-	}
-	if (py->months == NULL)
-		py->months = pm;
-
-	pdp = NULL;
-	pd = pm->days;
-	while (pd != NULL) {
-		pdp = pd;
-		pd = pd->nextday;
-	}
-
-	if (pd == NULL) {	/* Always true */
-		pd = xcalloc(1, sizeof(*pd));
-		pd->month = pm;
-		pd->year = py;
-		pd->dayofmonth = d;
-		pd->julianday = pm->firstdayjulian + d - 1;
-		if (pdp != NULL)
-			pdp->nextday = pd;
-	}
-	if (pm->days == NULL)
-		pm->days = pd;
-}
 
 void
 generatedates(int rd1, int rd2)
 {
-	struct g_date gdate;
-	int y1, m1, d1;
-	int y2, m2, d2;
-	int y, m, d;
+	struct cal_day *dp;
+	struct cal_day **dpp = &cal_days;
 
-	gregorian_from_fixed(rd1, &gdate);
-	y1 = gdate.year;
-	m1 = gdate.month;
-	d1 = gdate.day;
-
-	gregorian_from_fixed(rd2, &gdate);
-	y2 = gdate.year;
-	m2 = gdate.month;
-	d2 = gdate.day;
-
-	if (y1 == y2) {
-		if (m1 == m2) {
-			/* Same year, same month. Easy! */
-			for (d = d1; d <= d2; d++)
-				createdate(y1, m1, d);
-			return;
-		}
-		/*
-		 * Same year, different month.
-		 * - Take the leftover days from m1
-		 * - Take all days from <m1 .. m2>
-		 * - Take the first days from m2
-		 */
-		monthdays = monthdaytab[isleap(y1)];
-		for (d = d1; d <= monthdays[m1]; d++)
-			createdate(y1, m1, d);
-		for (m = m1 + 1; m < m2; m++)
-			for (d = 1; d <= monthdays[m]; d++)
-				createdate(y1, m, d);
-		for (d = 1; d <= d2; d++)
-			createdate(y1, m2, d);
-		return;
+	for (int rd = rd1; rd <= rd2; rd++) {
+		dp = xcalloc(1, sizeof(*dp));
+		dp->rd = rd;
+		*dpp = dp;
+		dpp = &dp->next;
 	}
-	/*
-	 * Different year, different month.
-	 * - Take the leftover days from y1-m1
-	 * - Take all days from y1-<m1 .. 12]
-	 * - Take all days from <y1 .. y2>
-	 * - Take all days from y2-[1 .. m2>
-	 * - Take the first days of y2-m2
-	 */
-	monthdays = monthdaytab[isleap(y1)];
-	for (d = d1; d <= monthdays[m1]; d++)
-		createdate(y1, m1, d);
-	for (m = m1 + 1; m <= 12; m++)
-		for (d = 1; d <= monthdays[m]; d++)
-			createdate(y1, m, d);
-	for (y = y1 + 1; y < y2; y++) {
-		monthdays = monthdaytab[isleap(y)];
-		for (m = 1; m <= 12; m++)
-			for (d = 1; d <= monthdays[m]; d++)
-				createdate(y, m, d);
-	}
-	monthdays = monthdaytab[isleap(y2)];
-	for (m = 1; m < m2; m++)
-		for (d = 1; d <= monthdays[m]; d++)
-			createdate(y2, m, d);
-	for (d = 1; d <= d2; d++)
-		createdate(y2, m2, d);
 }
 
 void
 dumpdates(void)
 {
-	struct cal_year *yp;
-	struct cal_month *mp;
 	struct cal_day *dp;
-	struct g_date date;
-	int rd, dow;
+	int i, dow;
 
-	for (yp = hyear; yp != NULL; yp = yp->nextyear) {
-		date.year = yp->year;
-		date.month = 1;
-		date.day = 1;
-		rd = fixed_from_gregorian(&date);
-		dow = (int)dayofweek_from_fixed(rd);
-		fprintf(stderr, "%-5d (rd:%d, dow:%d)\n", yp->year, rd, dow);
-		for (mp = yp->months; mp != NULL; mp = mp->nextmonth) {
-			date.month = mp->month;
-			date.day = 1;
-			rd = fixed_from_gregorian(&date);
-			dow = (int)dayofweek_from_fixed(rd);
-			fprintf(stderr, "-- %-5d (rd:%d, dow:%d)\n",
-				mp->month, rd, dow);
-			for (dp = mp->days; dp != NULL; dp = dp->nextday) {
-				date.day = dp->dayofmonth;
-				rd = fixed_from_gregorian(&date);
-				dow = (int)dayofweek_from_fixed(rd);
-				fprintf(stderr, "  -- %-5d (rd:%d, dow:%d)\n",
-					dp->dayofmonth, rd, dow);
-			}
-		}
+	for (i = 0, dp = cal_days; dp != NULL; i++, dp = dp->next) {
+		dow = (int)dayofweek_from_fixed(dp->rd);
+		fprintf(stderr, "%s(): [%d] rd:%d, dow:%d\n",
+			__func__, i, dp->rd, dow);
 	}
 }
 
@@ -294,33 +140,17 @@ first_dayofweek_of_month(int yy, int mm)
 bool
 walkthrough_dates(struct event **e)
 {
-	static struct cal_year *y = NULL;
-	static struct cal_month *m = NULL;
-	static struct cal_day *d = NULL;
+	static struct cal_day *dp = NULL;
+	static bool inited = false;
 
-	if (y == NULL) {
-		y = hyear;
-		m = y->months;
-		d = m->days;
-		*e = d->events;
-		return true;
+	if (dp == NULL && !inited) {
+		dp = cal_days;
+		inited = true;
 	}
-	if (d->nextday != NULL) {
-		d = d->nextday;
-		*e = d->events;
-		return true;
-	}
-	if (m->nextmonth != NULL) {
-		m = m->nextmonth;
-		d = m->days;
-		*e = d->events;
-		return true;
-	}
-	if (y->nextyear != NULL) {
-		y = y->nextyear;
-		m = y->months;
-		d = m->days;
-		*e = d->events;
+
+	if (dp != NULL) {
+		*e = dp->events;
+		dp = dp->next;
 		return true;
 	}
 
@@ -330,22 +160,14 @@ walkthrough_dates(struct event **e)
 struct cal_day *
 find_yd(int yy, int dd)
 {
-	struct cal_year *yp;
-	struct cal_month *mp;
+	struct g_date gdate = { yy, 1, 1 };
 	struct cal_day *dp;
+	int rd;
 
-	for (yp = hyear; yp != NULL; yp = yp->nextyear) {
-		if (yp->year == yy)
-			break;
-	}
-	if (yp == NULL)
-		return NULL;
-
-	for (mp = yp->months; mp != NULL; mp = mp->nextmonth) {
-		for (dp = mp->days; dp != NULL; dp = dp->nextday) {
-			if (dp->julianday == dd)
-				return dp;
-		}
+	rd = fixed_from_gregorian(&gdate) + dd - 1;
+	for (dp = cal_days; dp != NULL; dp = dp->next) {
+		if (dp->rd == rd)
+			return dp;
 	}
 
 	return NULL;
@@ -354,26 +176,13 @@ find_yd(int yy, int dd)
 struct cal_day *
 find_ymd(int yy, int mm, int dd)
 {
-	struct cal_year *yp;
-	struct cal_month *mp;
+	struct g_date gdate = { yy, mm, dd };
 	struct cal_day *dp;
+	int rd;
 
-	for (yp = hyear; yp != NULL; yp = yp->nextyear) {
-		if (yp->year == yy)
-			break;
-	}
-	if (yp == NULL)
-		return NULL;
-
-	for (mp = yp->months; mp != NULL; mp = mp->nextmonth) {
-		if (mp->month == mm)
-			break;
-	}
-	if (mp == NULL)
-		return NULL;
-
-	for (dp = mp->days; dp != NULL; dp = dp->nextday) {
-		if (dp->dayofmonth == dd)
+	rd = fixed_from_gregorian(&gdate);
+	for (dp = cal_days; dp != NULL; dp = dp->next) {
+		if (dp->rd == rd)
 			return dp;
 	}
 
