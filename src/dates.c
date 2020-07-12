liweitianux/ccalendar

commit 768daaeccce573b33f915feec5c6495c8a97e522

dates.c: Rewrite first_dayofweek_of_{year,month}()
* Use new fixed_from_gregorian() and dayofweek_from_fiex() functions to
  calculate the day of week of the first day of the year (and the
  month), in preparing to convert the 'cal_{year,month,day}' structs to
  base on R.D.

* Add 'days_{before,after}' to 'struct cal_options' as required by the
  first_dayofweek_of_month() function.

src/dates.c
@@ -36,6 +36,8 @@
 #include <time.h>
 
 #include "calendar.h"
+#include "basics.h"
+#include "gregorian.h"
 #include "utils.h"
 
 struct cal_year {
@@ -266,35 +268,29 @@ dumpdates(void)
 int
 first_dayofweek_of_year(int yy)
 {
-	struct cal_year *yp;
+	if (yy < Options.year1 || yy > Options.year2)
+		return -1;  /* out-of-range */
 
-	for (yp = hyear; yp != NULL; yp = yp->nextyear) {
-		if (yp->year == yy)
-			return yp->firstdayofweek;
-	}
-
-	return -1;
+	struct g_date date = { yy, 1, 1 };
+	int rd = fixed_from_gregorian(&date);
+	return (int)dayofweek_from_fixed(rd);
 }
 
 int
 first_dayofweek_of_month(int yy, int mm)
 {
-	struct cal_year *yp;
-	struct cal_month *mp;
+	int firstday, lastday;
 
-	for (yp = hyear; yp != NULL; yp = yp->nextyear) {
-		if (yp->year == yy)
-			break;
-	}
-	if (yp == NULL)
-		return -1;
+	struct g_date date = { yy, mm, 1 };
+	firstday = fixed_from_gregorian(&date);
+	date.month++;
+	lastday = fixed_from_gregorian(&date) - 1;
 
-	for (mp = yp->months; mp != NULL; mp = mp->nextmonth) {
-		if (mp->month == mm)
-			return mp->firstdayofweek;
-	}
+	if (firstday < (Options.today - Options.days_before) ||
+	    lastday > (Options.today + Options.days_after))
+		return -1;  /* out-of-range */
 
-	return -1;
+	return (int)dayofweek_from_fixed(firstday);
 }
 
 bool

src/calendar.c
@@ -52,6 +52,8 @@
 
 struct cal_options Options = {
 	.calendarFile = "calendar",  /* name of default calendar file */
+	.days_before = 0,
+	.days_after = 0,
 	.UTCOffset = 0.0,
 	.EastLongitude = 0.0,
 	.debug = false,
@@ -66,9 +68,7 @@ main(int argc, char *argv[])
 {
 	bool	doall = false;
 	int	ret = 0;
-	int	f_dayAfter = 0;		/* days after current date */
-	int	f_dayBefore = 0;	/* days before current date */
-	int	Friday = 5;		/* days before weekend */
+	int	Friday = 5;  /* days before weekend */
 	int	ch;
 	enum dayofweek dow;
 	struct g_date gdate1, gdate2;
@@ -92,14 +92,14 @@ main(int argc, char *argv[])
 			Friday = -1;
 			/* FALLTHROUGH */
 		case 'A': /* days after current date */
-			f_dayAfter = atoi(optarg);
-			if (f_dayAfter < 0)
+			Options.days_after = atoi(optarg);
+			if (Options.days_after < 0)
 				errx(1, "number of days must be positive");
 			break;
 
 		case 'B': /* days before current date */
-			f_dayBefore = atoi(optarg);
-			if (f_dayBefore < 0)
+			Options.days_before = atoi(optarg);
+			if (Options.days_before < 0)
 				errx(1, "number of days must be positive");
 			break;
 
@@ -146,12 +146,12 @@ main(int argc, char *argv[])
 
 	/* Friday displays Monday's events */
 	dow = dayofweek_from_fixed(Options.today);
-	if (f_dayAfter == 0 && Friday != -1)
-		f_dayAfter = ((int)dow == Friday) ? 3 : 1;
+	if (Options.days_after == 0 && Friday != -1)
+		Options.days_after = ((int)dow == Friday) ? 3 : 1;
 
-	gregorian_from_fixed(Options.today - f_dayBefore, &gdate1);
+	gregorian_from_fixed(Options.today - Options.days_before, &gdate1);
 	Options.year1 = gdate1.year;
-	gregorian_from_fixed(Options.today + f_dayAfter, &gdate2);
+	gregorian_from_fixed(Options.today + Options.days_after, &gdate2);
 	Options.year2 = gdate2.year;
 
 	generatedates(&gdate1, &gdate2);

src/calendar.h
@@ -69,6 +69,8 @@
 struct cal_options {
 	const char *calendarFile;  /* name of calendar file */
 	int today;  /* R.D. of today to remind events */
+	int days_before;  /* number of days before today to remind events */
+	int days_after;  /* number of days after today to remind events */
 	int year1;  /* year of the beginning day */
 	int year2;  /* year of the ending day */
 	double UTCOffset;

commit ff58f64038dc9f61299045a003db2e444aa4bbca

dates.c: Fix date range bug in first_dayofweek_of_month()
This bug was introduced in commit
768daaeccce573b33f915feec5c6495c8a97e522, which broke the calculation
of 'day of week of month', e.g., 'Jan/Sat'.

src/dates.c
@@ -130,8 +130,8 @@ first_dayofweek_of_month(int yy, int mm)
 	date.month++;
 	lastday = fixed_from_gregorian(&date) - 1;
 
-	if (firstday < (Options.today - Options.days_before) ||
-	    lastday > (Options.today + Options.days_after))
+	if (firstday > (Options.today + Options.days_after) ||
+	    lastday < (Options.today - Options.days_before))
 		return -1;  /* out-of-range */
 
 	return (int)dayofweek_from_fixed(firstday);