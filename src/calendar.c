liweitianux/ccalendar

commit e176a4bfbeef4cba2381734fcb7c532fda40b08b

io.c: Move calendar file finding & opening logic to main()
In one hand, this rearrangement helps to optimize the '-a' mode
processing by avoiding unnecessay forks (i.e., if the user doesn't have
a calendar file configured at '~/.calendar/calendar', just skip him/her
and no need to fork).

On the other hand, this makes the calendar file finding procedure more
clear.

Partially refers to OpenBSD's calendar(1).

src/calendar.c
@@ -32,6 +32,7 @@
  * $FreeBSD: head/usr.bin/calendar/calendar.c 326025 2017-11-20 19:49:47Z pfg $
  */
 
+#include <sys/param.h>
 #include <sys/types.h>
 
 #include <err.h>
@@ -55,16 +56,31 @@
 
 
 struct cal_options Options = {
-	.calendarFile = NULL,
 	.time = 0.5,  /* noon */
 	.allmode = false,
 	.debug = false,
 };
 
-static void	usage(const char *progname) __dead2;
-static double	get_time_of_now(void);
+/* paths to search for calendar files for inclusion */
+const char *calendarDirs[] = {
+	".calendar",  /* relative to $HOME */
+	CALENDAR_ETCDIR,
+	CALENDAR_DIR,
+	NULL,
+};
+
+/* default calendar file to use if exists in current dir or ~/.calendar */
+static const char *calendarFile = "calendar";
+/* systemd-wide calendar file to use if user doesn't have one */
+static const char *calendarFileSys = CALENDAR_ETCDIR "/default";
+/* don't send mail if this file exists in ~/.calendar */
+static const char *calendarNoMail = "nomail";
+
+static void	cd_home(void);
 static int	get_fixed_of_today(void);
+static double	get_time_of_now(void);
 static int	get_utc_offset(void);
+static void	usage(const char *progname) __dead2;
 
 
 int
@@ -75,12 +91,14 @@ main(int argc, char *argv[])
 	int	days_before = 0;
 	int	days_after = 0;
 	int	Friday = 5;  /* days before weekend */
-	int	ch;
-	int	utc_offset;
+	int	ch, utc_offset;
 	enum dayofweek dow;
 	struct passwd *pw;
 	struct location loc = { 0 };
 	const char *show_info = NULL;
+	const char *calfile = NULL;
+	char path[MAXPATHLEN];
+	FILE *fp = NULL;
 
 	Options.location = &loc;
 	Options.time = get_time_of_now();
@@ -120,9 +138,9 @@ main(int argc, char *argv[])
 			break;
 
 		case 'f': /* other calendar file */
-			Options.calendarFile = optarg;
+			calfile = optarg;
 			if (strcmp(optarg, "-") == 0)
-				Options.calendarFile = "/dev/stdin";
+				calfile = "/dev/stdin";
 			break;
 
 		case 'L': /* location */
@@ -157,7 +175,7 @@ main(int argc, char *argv[])
 	if (argc > optind)
 		usage(argv[0]);
 
-	if (Options.allmode && Options.calendarFile != NULL)
+	if (Options.allmode && calfile != NULL)
 		errx(1, "flags -a and -f cannot be used together");
 
 	if (!L_flag)
@@ -203,7 +221,16 @@ main(int argc, char *argv[])
 
 	if (Options.allmode) {
 		while ((pw = getpwent()) != NULL) {
-			if (chdir(pw->pw_dir) == -1)
+			/*
+			 * Only try '~/.calendar/calendar'
+			 */
+			snprintf(path, sizeof(path), "%s/%s",
+				 pw->pw_dir, calendarDirs[0]);
+			if (chdir(path) == -1)
+				continue;
+			if (access(calendarNoMail, F_OK) == 0)
+				continue;
+			if ((fp = fopen(calendarFile, "r")) == NULL)
 				continue;
 
 			pid_t pid = fork();
@@ -217,12 +244,35 @@ main(int argc, char *argv[])
 				if (setuid(pw->pw_uid) == -1)
 					err(1, "setuid(%d)", (int)pw->pw_uid);
 
-				ret = cal();
+				ret = cal(fp);
+				fclose(fp);
 				_exit(ret);
 			}
 		}
 	} else {
-		ret = cal();
+		if (calfile && (fp = fopen(calfile, "r")) == NULL)
+			errx(1, "Cannot open calendar file: '%s'", calfile);
+
+		/* try 'calendar' in current directory */
+		if (fp == NULL)
+			fp = fopen(calendarFile, "r");
+		/* try '~/.calendar/calendar' */
+		if (fp == NULL) {
+			cd_home();
+			fp = fopen(calendarFile, "r");
+		}
+		/* fallback to '/etc/calendar/default' */
+		if (fp == NULL) {
+			warnx("No user's calendar file; "
+			      "fallback to system default: '%s'",
+			      calendarFileSys);
+			fp = fopen(calendarFileSys, "r");
+			if (fp == NULL)
+				errx(1, "Cannot find calendar file");
+		}
+
+		ret = cal(fp);
+		fclose(fp);
 	}
 
 	return (ret);
@@ -271,6 +321,21 @@ get_utc_offset(void)
 	localtime_r(&now, &tm);
 
 	return tm.tm_gmtoff;
+}
+
+static void
+cd_home(void)
+{
+	char *home;
+	char path[MAXPATHLEN];
+
+	home = getenv("HOME");
+	if (home == NULL || *home == '\0')
+		errx(1, "Cannot get '$HOME'");
+
+	snprintf(path, sizeof(path), "%s/%s", home, calendarDirs[0]);
+	if (chdir(path) != 0)
+		errx(1, "Cannot enter home directory: '%s'", home);
 }
 
 static void __dead2

src/calendar.h
@@ -56,7 +56,6 @@ struct location;
 
 struct cal_options {
 	struct location *location;
-	const char *calendarFile;  /* name of calendar file */
 	double time;  /* [0, 1) time of now in unit of days */
 	int today;  /* R.D. of today to remind events */
 	int day_begin;  /* beginning of date range to remind events */
@@ -68,6 +67,7 @@ struct cal_options {
 };
 
 extern struct cal_options Options;
+extern const char *calendarDirs[];  /* paths to search for calendar files */
 
 
 static inline bool
@@ -108,7 +108,7 @@ void	setnsequences(const char *seq);
 extern char *neaster, *npaskha, *ncny, *nfullmoon, *nnewmoon;
 extern char *nmarequinox, *nsepequinox, *njunsolstice, *ndecsolstice;
 
-int	cal(void);
+int	cal(FILE *fp);
 
 /* dates.c */
 struct cal_day;

src/io.c
@@ -56,19 +56,6 @@
 
 enum { C_NONE, C_LINE, C_BLOCK };
 
-/* default calendar file to use if exists in current dir or ~/.calendar */
-static const char *calendarFile = "calendar";
-/* systemd-wide calendar file to use if user doesn't have one */
-static const char *calendarFileSys = CALENDAR_ETCDIR "/default";
-/* paths to search for calendar files for inclusion */
-static const char *calendarDirs[] = {
-	".calendar",  /* relative to $HOME */
-	CALENDAR_ETCDIR,
-	CALENDAR_DIR,
-};
-/* don't send mail if this file exists in ~/.calendar */
-static const char *calendarNoMail = "nomail";
-
 static struct node *definitions = NULL;
 
 /* National names for special days */
@@ -84,9 +71,7 @@ char *ndecsolstice = NULL;
 
 static FILE	*cal_fopen(const char *file);
 static bool	 cal_parse(FILE *in);
-static void	 cd_home(void);
 static void	 closecal(FILE *fp);
-static FILE	*opencalin(void);
 static char	*skip_comment(char *line, int *comment);
 static bool	 tokenize(char *line, bool *skip);
 static void	 write_mailheader(FILE *fp);
@@ -138,9 +123,8 @@ cal_fopen(const char *file)
 {
 	FILE *fp = NULL;
 	char fpath[MAXPATHLEN];
-	size_t i;
 
-	for (i = 0; i < nitems(calendarDirs); i++) {
+	for (size_t i = 0; calendarDirs[i] != NULL; i++) {
 		snprintf(fpath, sizeof(fpath), "%s/%s",
 			 calendarDirs[i], file);
 		if ((fp = fopen(fpath, "r")) != NULL)
@@ -401,28 +385,22 @@ cal_parse(FILE *in)
 }
 
 int
-cal(void)
+cal(FILE *fpin)
 {
-	FILE *fpin;
-	FILE *fpout;
-
-	if ((fpin = opencalin()) == NULL)
-		return 1;
-
 	if (!cal_parse(fpin)) {
 		warnx("Failed to parse calendar files");
-		fclose(fpin);
 		return 1;
 	}
 
 	if (Options.allmode) {
+		FILE *fpout;
+
 		/*
 		 * Use a temporary output file, so we can skip sending mail
 		 * if there is no output.
 		 */
 		if ((fpout = tmpfile()) == NULL) {
 			warn("tmpfile");
-			fclose(fpin);
 			return 1;
 		}
 		event_print_all(fpout);
@@ -431,48 +409,10 @@ cal(void)
 		event_print_all(stdout);
 	}
 
-	fclose(fpin);
 	list_freeall(definitions, free, NULL);
 	definitions = NULL;
 
 	return 0;
-}
-
-static FILE *
-opencalin(void)
-{
-	FILE *fpin = NULL;
-	const char *calfile;
-	char fpath[MAXPATHLEN];
-
-	if (Options.allmode) {
-		/* already in $HOME, cd to '~/.calendar' */
-		if (chdir(calendarDirs[0]) == -1)
-			return (NULL);
-		if (access(calendarNoMail, F_OK) == 0)
-			return (NULL);
-
-		/* only try '~/.calendar/calendar' */
-		return fopen(calendarFile, "r");
-	}
-
-	calfile = Options.calendarFile ? Options.calendarFile : calendarFile;
-	fpin = fopen(calfile, "r");
-	if (fpin == NULL) {
-		if (Options.calendarFile)
-			errx(1, "No calendar file: '%s'", calfile);
-
-		cd_home();
-		snprintf(fpath, sizeof(fpath), "%s/%s",
-			 calendarDirs[0], calendarFile);
-		if ((fpin = fopen(fpath, "r")) == NULL &&
-		    (fpin = fopen(calendarFileSys, "r")) == NULL) {
-			errx(1, "No calendar file: '%s' or '~/%s'",
-					calendarFile, fpath);
-		}
-	}
-
-	return (fpin);
 }
 
 static void
@@ -552,16 +492,4 @@ write_mailheader(FILE *fp)
 		"Auto-Submitted: auto-generated\n\n",
 		pw->pw_name, pw->pw_name, dayname);
 	fflush(fp);
-}
-
-static void
-cd_home(void)
-{
-	char *home;
-
-	home = getenv("HOME");
-	if (home == NULL || *home == '\0')
-		errx(1, "Cannot get home directory");
-	if (chdir(home) != 0)
-		errx(1, "Cannot enter home directory: \"%s\"", home);
 }

commit c26b5b2559e18091427d962bdef8da3cd815a911

calendar.c: Fix calendar file finding bug
The last commit e176a4bfbeef4cba2381734fcb7c532fda40b08b introduced a
bug that caused the calendar files in ~/.calendar were not found.

To fix this bug, always enter '~/.calendar' in main() and change
'.calendar' to '.' in calendarDirs.

src/calendar.c
@@ -63,12 +63,14 @@ struct cal_options Options = {
 
 /* paths to search for calendar files for inclusion */
 const char *calendarDirs[] = {
-	".calendar",  /* relative to $HOME */
+	".",  /* i.e., '~/.calendar' */
 	CALENDAR_ETCDIR,
 	CALENDAR_DIR,
 	NULL,
 };
 
+/* user's calendar home directory (relative to $HOME) */
+static const char *calendarHome = ".calendar";
 /* default calendar file to use if exists in current dir or ~/.calendar */
 static const char *calendarFile = "calendar";
 /* systemd-wide calendar file to use if user doesn't have one */
@@ -222,10 +224,10 @@ main(int argc, char *argv[])
 	if (Options.allmode) {
 		while ((pw = getpwent()) != NULL) {
 			/*
-			 * Only try '~/.calendar/calendar'
+			 * Enter '~/.calendar' and only try 'calendar'
 			 */
 			snprintf(path, sizeof(path), "%s/%s",
-				 pw->pw_dir, calendarDirs[0]);
+				 pw->pw_dir, calendarHome);
 			if (chdir(path) == -1)
 				continue;
 			if (access(calendarNoMail, F_OK) == 0)
@@ -256,11 +258,11 @@ main(int argc, char *argv[])
 		/* try 'calendar' in current directory */
 		if (fp == NULL)
 			fp = fopen(calendarFile, "r");
+
+		cd_home();
 		/* try '~/.calendar/calendar' */
-		if (fp == NULL) {
-			cd_home();
+		if (fp == NULL)
 			fp = fopen(calendarFile, "r");
-		}
 		/* fallback to '/etc/calendar/default' */
 		if (fp == NULL) {
 			warnx("No user's calendar file; "
@@ -333,7 +335,7 @@ cd_home(void)
 	if (home == NULL || *home == '\0')
 		errx(1, "Cannot get '$HOME'");
 
-	snprintf(path, sizeof(path), "%s/%s", home, calendarDirs[0]);
+	snprintf(path, sizeof(path), "%s/%s", home, calendarHome);
 	if (chdir(path) != 0)
 		errx(1, "Cannot enter home directory: '%s'", home);
 }