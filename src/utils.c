5373842e1797326f51b483ae9acf99343730a6d9
Aaron LI <aly@aaronly.me>
2020-08-15 23:23:07 +0800

    utils.[ch]: Inline various small functions

--- src/utils.c
+++ src/utils.c
@@ -39,9 +39,7 @@
  * 2018, Cambridge University Press
  */
 
-#include <ctype.h>
 #include <err.h>
-#include <errno.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -51,70 +49,6 @@
 
 #include "utils.h"
 
-static const double eps = 1e-6;
-
-/*
- * Divide integer $x by integer $y, rounding towards minus infinity.
- */
-int
-div_floor(int x, int y)
-{
-	int q = x / y;
-	int r = x % y;
-	if ((r != 0) && ((r < 0) != (y < 0)))
-		q--;
-	return q;
-}
-
-/*
- * Calculate the remainder of $x divided by $y; the result has the same
- * sign as $y.
- * Ref: Sec.(1.7), Eq.(1.17)
- */
-int
-mod(int x, int y)
-{
-	return x - y * div_floor(x, y);
-}
-
-double
-mod_f(double x, double y)
-{
-	return x - y * floor(x / y);
-}
-
-/*
- * Return the value of ($x % $y) with $y instead of 0, i.e., with value
- * range being [1, $y].
- */
-int
-mod1(int x, int y)
-{
-	return y + mod(x, -y);
-}
-
-/*
- * Calculate the interval modulus of $x, i.e., shifted into the range
- * [$a, $b).  Return $x if $a = $b.
- * Ref: Sec.(1.7), Eq.(1.24)
- */
-int
-mod3(int x, int a, int b)
-{
-	if (a == b)
-		return x;
-	else
-		return a + mod(x - a, b - a);
-}
-
-double
-mod3_f(double x, double a, double b)
-{
-	if (fabs(a - b) < eps)
-		return x;
-	else
-		return a + mod_f(x - a, b - a);
-}
 
 /*
  * Calculate the polynomial: c[0] + c[1] * x + ... + c[n-1] * x^(n-1)
@@ -132,74 +66,6 @@ poly(double x, const double *coefs, size_t n)
 }
 
 /*
- * Calculate the sine value of degree $deg.
- */
-double
-sin_deg(double deg)
-{
-	return sin(M_PI * deg / 180.0);
-}
-
-/*
- * Calculate the cosine value of degree $deg.
- */
-double
-cos_deg(double deg)
-{
-	return cos(M_PI * deg / 180.0);
-}
-
-/*
- * Calculate the tangent value of degree $deg.
- */
-double
-tan_deg(double deg)
-{
-	return tan(M_PI * deg / 180.0);
-}
-
-/*
- * Calculate the arc sine value (in degrees) of $x.
- */
-double
-arcsin_deg(double x)
-{
-	return asin(x) * 180.0 / M_PI;
-}
-
-/*
- * Calculate the arc cosine value (in degrees) of $x.
- */
-double
-arccos_deg(double x)
-{
-	return acos(x) * 180.0 / M_PI;
-}
-
-/*
- * Calculate the arc tangent value (in degrees from 0 to 360) of $y / $x.
- * Error if $x and $y are both zero.
- */
-double
-arctan_deg(double y, double x)
-{
-	errno = 0;
-	double v = atan2(y, x);
-	if (errno == EDOM)
-		errx(10, "%s(%g, %g) invalid!", __func__, y, x);
-	return mod_f(v * 180.0 / M_PI, 360);
-}
-
-/*
- * Convert angle in (degree, arcminute, arcsecond) to degree.
- */
-double
-angle2deg(int deg, int min, double sec)
-{
-	return deg + min/60.0 + sec/3600.0;
-}
-
-/*
  * Use bisection search to find the inverse of the given angular function
  * $f(x) at value $y (degrees) within time interval [$a, $b].
  * Ref: Sec.(1.8), Eq.(1.36)
@@ -207,6 +73,7 @@ angle2deg(int deg, int min, double sec)
 double
 invert_angular(double (*f)(double), double y, double a, double b)
 {
+	static const double eps = 1e-6;
 	double x;
 
 	do {
@@ -267,44 +134,6 @@ xstrdup(const char *str)
 	if (p == NULL)
 		errx(1, "xstrdup: out of memory (length: %zu)", strlen(str));
 	return p;
-}
-
-/*
- * Count the number of character $ch in string $s.
- */
-size_t
-count_char(const char *s, int ch)
-{
-	size_t count = 0;
-
-	for ( ; *s; s++) {
-		if (*s == ch)
-			count++;
-	}
-
-	return count;
-}
-
-
-char *
-triml(char *s)
-{
-	while (isspace((unsigned char)*s))
-		s++;
-
-	return s;
-}
-
-char *
-trimr(char *s)
-{
-	size_t l = strlen(s);
-
-	while (l > 0 && isspace((unsigned char) s[l-1]))
-		l--;
-	s[l] = '\0';
-
-	return s;
 }
 
 
--- src/utils.h
+++ src/utils.h
@@ -37,6 +37,10 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
@@ -45,12 +49,65 @@
 #define nitems(x)	(sizeof(x) / sizeof((x)[0]))
 #endif
 
+
+/*
+ * Return true if string $s1 starts with the string $s2.
+ */
 static inline bool
 string_startswith(const char *s1, const char *s2)
 {
 	return (s1 && s2 && strncmp(s1, s2, strlen(s2)) == 0);
 }
 
+/*
+ * Count the number of character $ch in string $s.
+ */
+static inline size_t
+count_char(const char *s, int ch)
+{
+	size_t count = 0;
+
+	for ( ; *s; s++) {
+		if (*s == ch)
+			count++;
+	}
+
+	return count;
+}
+
+/*
+ * Trim the leading whitespaces of the given string $s
+ * and return the trimed string.
+ */
+static inline char *
+triml(char *s)
+{
+	while (isspace((unsigned char)*s))
+		s++;
+
+	return s;
+}
+
+/*
+ * Trim the trailing whitespaces of the given string $s
+ * and return the trimed string.
+ */
+static inline char *
+trimr(char *s)
+{
+	size_t l = strlen(s);
+
+	while (l > 0 && isspace((unsigned char) s[l-1]))
+		l--;
+	s[l] = '\0';
+
+	return s;
+}
+
+
+/*
+ * Swap the values of two integers.
+ */
 static inline void
 swap(int *a, int *b)
 {
@@ -59,37 +116,148 @@ swap(int *a, int *b)
 	*b = tmp;
 }
 
+/*
+ * Divide integer $x by integer $y, rounding towards minus infinity.
+ */
+static inline int
+div_floor(int x, int y)
+{
+	int q = x / y;
+	int r = x % y;
+	if ((r != 0) && ((r < 0) != (y < 0)))
+		q--;
+	return q;
+}
 
-int	div_floor(int x, int y);
+/*
+ * Calculate the remainder of $x divided by $y; the result has the same
+ * sign as $y.
+ * Ref: Sec.(1.7), Eq.(1.17)
+ */
+static inline int
+mod(int x, int y)
+{
+	return x - y * div_floor(x, y);
+}
 
-int	mod(int x, int y);
-double	mod_f(double x, double y);
-int	mod1(int x, int y);
-int	mod3(int x, int a, int b);
-double	mod3_f(double x, double a, double b);
+static inline double
+mod_f(double x, double y)
+{
+	return x - y * floor(x / y);
+}
+
+/*
+ * Return the value of ($x % $y) with $y instead of 0, i.e., with value
+ * range being [1, $y].
+ */
+static inline int
+mod1(int x, int y)
+{
+	return y + mod(x, -y);
+}
+
+/*
+ * Calculate the interval modulus of $x, i.e., shifted into the range
+ * [$a, $b).  Return $x if $a = $b.
+ * Ref: Sec.(1.7), Eq.(1.24)
+ */
+static inline int
+mod3(int x, int a, int b)
+{
+	if (a == b)
+		return x;
+	else
+		return a + mod(x - a, b - a);
+}
+
+static inline double
+mod3_f(double x, double a, double b)
+{
+	static const double eps = 1e-6;
+
+	if (fabs(a - b) < eps)
+		return x;
+	else
+		return a + mod_f(x - a, b - a);
+}
+
+
+/*
+ * Calculate the sine value of degree $deg.
+ */
+static inline double
+sin_deg(double deg)
+{
+	return sin(M_PI * deg / 180.0);
+}
+
+/*
+ * Calculate the cosine value of degree $deg.
+ */
+static inline double
+cos_deg(double deg)
+{
+	return cos(M_PI * deg / 180.0);
+}
+
+/*
+ * Calculate the tangent value of degree $deg.
+ */
+static inline double
+tan_deg(double deg)
+{
+	return tan(M_PI * deg / 180.0);
+}
+
+/*
+ * Calculate the arc sine value (in degrees) of $x.
+ */
+static inline double
+arcsin_deg(double x)
+{
+	return asin(x) * 180.0 / M_PI;
+}
+
+/*
+ * Calculate the arc cosine value (in degrees) of $x.
+ */
+static inline double
+arccos_deg(double x)
+{
+	return acos(x) * 180.0 / M_PI;
+}
+
+/*
+ * Calculate the arc tangent value (in degrees from 0 to 360) of $y / $x.
+ * Error if $x and $y are both zero.
+ */
+static inline double
+arctan_deg(double y, double x)
+{
+	errno = 0;
+	double v = atan2(y, x);
+	if (errno == EDOM)
+		errx(10, "%s(%g, %g) invalid!", __func__, y, x);
+	return mod_f(v * 180.0 / M_PI, 360);
+}
+
+/*
+ * Convert angle in (degree, arcminute, arcsecond) to degree.
+ */
+static inline double
+angle2deg(int deg, int min, double sec)
+{
+	return deg + min/60.0 + sec/3600.0;
+}
+
 
 double	poly(double x, const double *coefs, size_t n);
-
-double	sin_deg(double deg);
-double	cos_deg(double deg);
-double	tan_deg(double deg);
-double	arcsin_deg(double x);
-double	arccos_deg(double x);
-double	arctan_deg(double y, double x);
-
-double	angle2deg(int deg, int min, double sec);
-
 double	invert_angular(double (*f)(double), double y, double a, double b);
 
 void *	xmalloc(size_t size);
 void *	xcalloc(size_t number, size_t size);
 void *	xrealloc(void *ptr, size_t size);
 char *	xstrdup(const char *str);
-
-size_t	count_char(const char *s, int ch);
-
-char *	triml(char *s);
-char *	trimr(char *s);
 
 struct node;
 struct node *	list_newnode(char *name, void *data);
